92b7a3df801108e0b7c2f42fc4563c5992aba1fe
Sagi <sagi.laufer23@gmail.com>
2020-03-17 12:16:45 +0200

    redux is added

--- sagis-site/src/App.tsx
+++ sagis-site/src/App.tsx
@@ -1,18 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
+import { simpleAction } from "./actions/simpleAction";
+
+interface State {
+  currentProps: string;
 }
 
-export default App;
+interface Props {
+  simpleAction?: () => void;
+}
+class App extends Component<Props, State> {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <div>{JSON.stringify(this.props)}</div>
+        <button onClick={this._onClick}>Click Me!</button>
+      </div>
+    );
+  }
+
+  private _onClick = () => {
+    this.props.simpleAction && this.props.simpleAction();
+  };
+}
+
+const mapStateToProps = (state: any) => ({
+  ...state
+});
+
+const mapDispatchToProps = (dispatch: any) => ({
+  simpleAction: () => dispatch(simpleAction)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
